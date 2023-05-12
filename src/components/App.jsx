7f9722cd2b21d6ab1b8c584b6217116a096fa02e
ValeriiHol <rielty@gmail.com>
2023-05-12 14:56:19 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      =========  goit-react-hw-02-phonebook ==============
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
+import Container from 'components/Container/Container';
+import ContactForm from 'components/ContactForm/ContactForm';
 
+class App extends Component {
+  state = {
+    contacts: [
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const { contacts } = this.state;
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts.`);
+    } else if (contacts.find(contact => contact.number === number)) {
+      alert(`${number} is already in contacts.`);
+    } else if (name.trim() === '' || number.trim() === '') {
+      alert("Enter the contact's name and number phone!");
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+      </Container>
+    );
+  }
+}
+
+export default App;
 
 // // clo
 // console.log('object :>> ', object);
@@ -23,10 +58,10 @@ export const App = () => {
 // console.log(object);
 // //nfn
 // const name = (params) => {
-   
+
 // }
 
 // anfn
 // (params) => {
-  
+
 // }
